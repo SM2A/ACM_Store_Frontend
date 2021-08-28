38ccc68359e75539e7c504de72eeca619a871672
SM2A <seyedamin1990@gmail.com>
2021-08-28 16:02:36 +0430

    Admin authentication

---
+++ js/admin.js
@@ -0,0 +1,35 @@
+var email = getCookie("email");
+var password = getCookie("password");
+
+if (((email === "") && (password !== ""))
+    || ((email !== "") && (password === ""))
+    || ((email === "") && (password === ""))) {
+    alert("Please login first");
+    window.location.href = "/";
+    clearCookie();
+} else {
+    $.ajax({
+        url: "http://localhost:8080/valid_login",
+        type: "POST",
+        data: {"email": email, "password": password},
+        success: function (response) {
+            if (response == "0") {
+                alert("Please login first");
+                clearCookie();
+                window.location = "/static/Login.html";
+            } else if (response == "1") {
+                $.ajax({
+                    url: "http://localhost:8080/valid_admin",
+                    type: "POST",
+                    data: {"email": email, "password": password},
+                    success: function (response) {
+                        if (response == "0") {
+                            alert("You are not Admin")
+                            window.location = "/";
+                        }
+                    }
+                });
+            }
+        }
+    });
+}
